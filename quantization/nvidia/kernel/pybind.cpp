1eb237601d02ddc33c315f97ccb0ef6756f5c717
sparklesea <funsunlyl@gmail.com>
2024-04-25 14:23:20 +0800

    fix nvidia pybind

--- quantization/nvidia/kernel/pybind.cpp
+++ quantization/nvidia/kernel/pybind.cpp
@@ -6,4 +6,7 @@
 
 PYBIND11_MODULE(quant, m){
   m.def("gemm_awq_ut", &gemm_awq_ut);
+  m.def("transpose_merge_zeros_scales", &transpose_merge_zeros_scales);
+  m.def("convert_ours_to_awq", &convert_ours_to_awq);
+  m.def("convert_awq_to_lmdeploy", &convert_awq_to_lmdeploy);
 }
