371a42605abcb929073b565b27779b7d8b040590
xiakubaobaore <1052469954@qq.com>
2024-05-06 14:42:34 +0800

    add 8, 9 test, but not real sentence

---
+++ test/eval_origin_bert_support.py
@@ -0,0 +1,145 @@
+from transformers import AutoTokenizer
+from datasets import Dataset
+import argparse
+from torch.utils.data import DataLoader
+import torch
+import json
+from tqdm import tqdm
+from quantizer.bert_quantizer import BERTQuantizer
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--model_path", type=str, default="bert_model/bert-large-cased-lambada")
+parser.add_argument("--w_group_size", type=int, default=128)
+parser.add_argument("--w_bit", type=int, default=16)
+parser.add_argument("--a_group_size", type=int, default=128)
+parser.add_argument("--a_bit", type=int, default=16)
+parser.add_argument("--kv_group_size", type=int, default=64)
+parser.add_argument("--kv_bit", type=int, default=16)
+parser.add_argument("--mask_path", type=str, default=None)  # /share/liutengxuan/NLP-playground/examples/sparse_attention/model/bert/bigbird_pattern_24_16_512_512.pt")
+parser.add_argument('--lut_path', type=str, default=None)
+parser.add_argument("--output_path", type=str, help="path to save the quantized model")
+parser.add_argument("--quantized", action="store_true")
+parser.add_argument("--eval", action="store_true")
+parser.add_argument("--sample", nargs="+", type=int, default=[0, 5, 8, 15, 18])
+args = parser.parse_args()
+
+enc = AutoTokenizer.from_pretrained("bert-large-cased")
+
+def tokenize_function(examples):
+
+    inputs = enc(examples["sentence1"], examples["sentence2"], truncation=True)
+    return inputs
+
+dataset = {
+    "sentence1": [
+        "pre sentence 1", 
+        "pre sentence 2", 
+        "pre sentence 3", 
+        "pre sentence 4", 
+        "pre sentence 5", 
+        "pre sentence 6", 
+        "pre sentence 7", 
+        "pre sentence 8", 
+        "pre sentence 9", 
+        "pre sentence 10", 
+        "pre sentence 11", 
+        "pre sentence 12", 
+        "pre sentence 13", 
+        "pre sentence 14", 
+        "pre sentence 15", 
+        "pre sentence 16", 
+        "pre sentence 17", 
+        "pre sentence 18", 
+        "pre sentence 19", 
+        "pre sentence 20"
+    ], 
+    "sentence2": [
+        "next sentence 1", 
+        "next sentence 2", 
+        "next sentence 3", 
+        "next sentence 4", 
+        "next sentence 5", 
+        "next sentence 6", 
+        "next sentence 7", 
+        "next sentence 8", 
+        "next sentence 9", 
+        "next sentence 10", 
+        "next sentence 11", 
+        "next sentence 12", 
+        "next sentence 13", 
+        "next sentence 14", 
+        "next sentence 15", 
+        "next sentence 16", 
+        "next sentence 17", 
+        "next sentence 18", 
+        "next sentence 19", 
+        "next sentence 20"
+    ],
+    "labels": [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1]
+}
+
+eval_dataset = Dataset.from_dict(dataset)
+eval_dataset = eval_dataset[args.sample]
+eval_dataset = Dataset.from_dict(eval_dataset)
+print(eval_dataset)
+
+def collate_fn(data):
+    tensor_input_ids, tensor_token_type_ids, tensor_attention_mask, idx, labels = [], [], [], [], []
+    for item in data:
+        tensor_input_ids.append(item["input_ids"])
+        tensor_token_type_ids.append(item["token_type_ids"])
+        tensor_attention_mask.append(item["attention_mask"])
+        # idx.append(item["idx"])
+        labels.append(item["labels"])
+        tensor_input_ids = torch.tensor(tensor_input_ids, dtype=int).cuda()
+        tensor_token_type_ids = torch.tensor(tensor_token_type_ids, dtype=int).cuda()
+        tensor_attention_mask = torch.tensor(tensor_attention_mask, dtype=int).cuda()
+    # idx = torch.tensor(idx, dtype=int).cuda()
+    labels = torch.tensor(labels, dtype=int).cuda()
+    return {"labels": labels, "input_ids": tensor_input_ids, "token_type_ids": tensor_token_type_ids, "attention_mask": tensor_attention_mask}
+
+eval_dataset = eval_dataset.map(tokenize_function, batched=True).shuffle(seed=42)
+eval_dataloader = DataLoader(eval_dataset, batch_size=1, collate_fn=collate_fn)
+
+if not args.quantized:
+    from transformers import BertForNextSentencePrediction
+else:
+    from module.bert.modeling_bert_ours import BertForNextSentencePrediction
+
+kwargs = {"torch_dtype": torch.float16}
+model = BertForNextSentencePrediction.from_pretrained(args.model_path, **kwargs)
+if args.mask_path is not None:
+    from module.bert.modeling_bert import BertModel_use_static_attention
+
+    model.bert.use_static_attention = BertModel_use_static_attention.__get__(model.bert)
+    model.bert.use_static_attention()
+    print("Using sparse mask {}".format(args.mask_path))
+    model.bert.set_static_attention_mask(args.mask_path)
+if args.lut_path is not None:
+    from module.bert.modeling_bert import BertModel_use_block_sparse_attention_lut
+    from module.mask.sparse_attention import set_static_attention_lut
+
+    model.bert.use_static_attention = BertModel_use_block_sparse_attention_lut.__get__(model.bert)
+    model.bert.use_static_attention()
+    print("Using sparse lut {}".format(args.lut_path))
+    set_static_attention_lut(args.lut_path, None, model.bert.encoder.layer, 64)
+
+model = model.to("cuda")
+if not args.quantized:
+    quantizer=BERTQuantizer(w_bit=args.w_bit,a_bit=args.a_bit,w_group_size=args.w_group_size)
+    model = quantizer(model)
+    print(model)
+    if args.output_path:
+        model.save_pretrained(args.output_path, safe_serialization=True)
+        # enc.torch_dtype="float16"
+        enc.save_pretrained(args.output_path, safe_serialization=True)
+model.eval()
+
+
+with torch.no_grad():
+    for batch in tqdm(eval_dataloader):
+        logits = model(**batch)[1]
+        pred = torch.argmax(logits, dim=-1)
+        print(pred)
+
---
+++ test/utils/quant_dequant_nvidia.py
@@ -0,0 +1,148 @@
+import torch
+from quant import transpose_merge_zeros_scales, convert_ours_to_awq, convert_awq_to_lmdeploy
+
+# W4A16 reformat quant param
+def quant_lmdeploy(weight, input_features, output_features, group_size, n_bit):
+
+    qweight, zeros, scales, w_fp16 = generate_quant(weight, input_features, n_bit, group_size)
+
+    zeros_scales = torch.empty((input_features // group_size, output_features * 2), dtype=torch.float16, device="cuda")
+    transpose_merge_zeros_scales(zeros, 
+                                scales,
+                                zeros_scales, 
+                                input_features,
+                                output_features,
+                                group_size)
+
+    qweight_awq = torch.empty((input_features, output_features // 2), dtype=torch.uint8, device="cuda")
+    convert_ours_to_awq(qweight, 
+                        qweight_awq, 
+                        input_features, 
+                        output_features)
+
+    qweight_lmdeploy = torch.empty((input_features, output_features // 8), dtype=torch.int, device="cuda")
+    convert_awq_to_lmdeploy(qweight_awq,
+                            qweight_lmdeploy,
+                            input_features,
+                            output_features)
+    
+    if n_bit == 2:
+        qweight_lmdeploy = convert_4bit_to_2bit(qweight_lmdeploy)
+
+    return qweight_lmdeploy, zeros_scales, w_fp16
+
+def convert_4bit_to_2bit(qweight_4bit):
+    origin_shape = qweight_4bit.shape
+    qweight_2bit = torch.zeros((origin_shape[0] * origin_shape[1] // 2), dtype=torch.int, device=qweight_4bit.device)
+    qweight_4bit_flatten = qweight_4bit.view(-1, 2)
+
+    for i in range(1, 9):
+        qweight_2bit[:] += (qweight_4bit_flatten[:, 0] & (0x30000000 >> 4 * (i - 1)) << 2 * i)
+        qweight_2bit[:] += (qweight_4bit_flatten[:, 1] & (0x30000000 >> 4 * (i - 1)) >> 16 - 2 * i)
+
+    return qweight_2bit.reshape(origin_shape[0], origin_shape[1] // 2)
+
+def convert_2bit_to_4bit(qweight_2bit):
+    origin_shape = qweight_2bit.shape
+    qweight_4bit = torch.zeros((origin_shape[0] * origin_shape[1], 2), dtype=torch.int, device=qweight_2bit.device)
+    qweight_2bit_flatten = qweight_2bit.view(-1)
+
+    for i in range(1, 9):
+        qweight_4bit[:, 0] += (qweight_2bit_flatten[:] & (0xC0000000 >> 2 * (i - 1)) >> 2 * i)
+        qweight_4bit[:, 1] += (qweight_2bit_flatten[:] & (0x0000C000 >> 2 * (i - 1)) << 16 - 2 * i)
+
+    return qweight_4bit.reshape(origin_shape[0], origin_shape[1] * 2)
+
+def quantize_tensor(w, n_bit, q_group_size):
+    org_w_shape = w.shape
+    if q_group_size > 0:
+        assert org_w_shape[-1] % q_group_size == 0
+        w = w.reshape(-1, q_group_size)
+    assert w.dim() == 2
+
+    max_val = w.amax(dim=1, keepdim=True)
+    min_val = w.amin(dim=1, keepdim=True)
+    max_int = 2 ** n_bit - 1
+    min_int = 0
+    scales = (max_val - min_val).clamp(min=1e-5) / max_int
+    zeros = (-torch.round(min_val / scales)).clamp_(min_int, max_int)
+
+    w = torch.clamp(torch.round(w / scales) + zeros, min_int, max_int)
+    w_fp16 = (w - zeros) * scales
+    w = w.reshape(org_w_shape)
+    w_fp16 = w_fp16.reshape(org_w_shape)
+
+    scales = scales.reshape(org_w_shape[0], -1)
+    zeros = zeros.reshape(org_w_shape[0], -1)
+    return w, scales, zeros, w_fp16
+
+def write_4bit_tensor(w_q):
+    w_q_org_shape = w_q.shape
+    w_q = w_q.type(torch.uint8).reshape(-1, 2)
+    w_int4 = torch.zeros(w_q.shape[0], 1, dtype=torch.uint8, device=w_q.device)
+
+    w_int4[:, 0] = w_q[:, 0] << 4
+    w_int4[:, 0] += w_q[:, 1]
+
+    new_shape = w_q_org_shape[:-1] + (w_q_org_shape[-1] * 4 // 8,)
+    return w_int4.reshape(new_shape)
+
+def write_2bit_tensor(w_q):
+    w_q_org_shape = w_q.shape
+    w_q = w_q.type(torch.uint8).reshape(-1, 4)
+    w_int2 = torch.zeros(w_q.shape[0], 1, dtype=torch.uint8, device=w_q.device)
+
+    w_int2[:, 0] = w_q[:, 0] << 6
+    w_int2[:, 0] += (w_q[:, 1] << 4)
+    w_int2[:, 0] += (w_q[:, 2] << 2)
+    w_int2[:, 0] += w_q[:, 3]
+
+    new_shape = w_q_org_shape[:-1] + (w_q_org_shape[-1] * 2 // 8,)
+    return w_int2.reshape(new_shape)
+
+def generate_quant(weight_fp16, input_features, n_bit, group_size):
+    assert (n_bit == 4 or n_bit == 2)
+
+    # quantize weight
+    intweight = []
+    zeros = []
+    scales = []
+
+    qweight_2bit, scales_temp, zeros_temp, w_fp16 = quantize_tensor(weight_fp16, n_bit, group_size)
+
+    intweight.append(write_4bit_tensor(qweight_2bit))
+
+    scales.append(scales_temp)
+    zeros.append(zeros_temp)
+
+    scales = torch.cat(scales, dim=1).clone().half()
+    qweight = torch.cat(intweight, dim=1).clone()
+    zeros = torch.cat(zeros, dim=1).clone().half()
+
+    return qweight, zeros, scales, w_fp16
+
+def quantize_tensor(w, n_bit, q_group_size):
+    # N, K
+    org_w_shape = w.shape
+    if q_group_size > 0:
+        assert org_w_shape[-1] % q_group_size == 0
+        w = w.reshape(-1, q_group_size)
+    assert w.dim() == 2
+
+    max_val = w.amax(dim=1, keepdim=True)
+    min_val = w.amin(dim=1, keepdim=True)
+    max_int = 2 ** n_bit - 1
+    min_int = 0
+    scales = (max_val - min_val).clamp(min=1e-5) / max_int
+    zeros = (-torch.round(min_val / scales)).clamp_(min_int, max_int)
+
+    w = torch.clamp(torch.round(w / scales) + zeros, min_int, max_int)
+    w_fp16 = (w - zeros) * scales
+
+    w = w.reshape(org_w_shape)
+    w_fp16 = w_fp16.reshape(org_w_shape)
+
+    scales = scales.reshape(org_w_shape[0], -1).contiguous()
+    zeros = zeros.reshape(org_w_shape[0], -1).contiguous()
+
+    return w, scales, zeros, w_fp16
