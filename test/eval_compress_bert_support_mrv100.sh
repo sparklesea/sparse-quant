256f3011c9a66a09854597a101d08370aaf06f20
xiakubaobaore <1052469954@qq.com>
2024-05-09 16:13:44 +0800

    add 20 bert sample to tianshu

--- test/eval_compress_bert_support_mrv100.sh
+++ test/eval_compress_bert_support_mrv100.sh
@@ -1,2 +1,2 @@
 
-python tianshu/bert_infer.py --model_path quantized_model/bert --w_bit 4 --lut_path masks/bert_large_lut.pt --quantized --eval
+python tianshu/bert_infer.py --model_path quantized_model/bert --w_bit 4 --lut_path masks/bert_large_lut.pt --quantized --eval --sample 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19
